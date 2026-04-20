cd3b0c9a913057dbe1fb9ce2e930a60d56381fa4
KunjKhimani <kunjkhimani@logicgoinfotech.in>
2026-04-20 12:52:55 +0530

    changes json

--- server/src/config/firebase-admin.config.ts
+++ server/src/config/firebase-admin.config.ts
@@ -2,9 +2,16 @@ import admin from "firebase-admin";
 import path from "path";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./dist/config/reyuDiamondKey.json", "utf-8")
-);
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const serviceAccount = process.env["FIREBASE_SERVICE_ACCOUNT"]
+  ? JSON.parse(process.env["FIREBASE_SERVICE_ACCOUNT"])
+  : JSON.parse(
+      fs.readFileSync(path.join(__dirname, "reyuDiamondKey.json"), "utf-8")
+    );
 
 type Bucket = ReturnType<admin.storage.Storage["bucket"]>;
 
